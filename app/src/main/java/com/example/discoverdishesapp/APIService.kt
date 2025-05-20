13b5802636b9197f6832aa7176459a867e9ee196
oscarAndroid <oscarlimachitapia@gmail.com>
2025-05-20 19:09:44 +0200

    hacemos api service

--- app/src/main/java/com/example/discoverdishesapp/APIService.kt
+++ app/src/main/java/com/example/discoverdishesapp/APIService.kt
@@ -1,8 +1,22 @@
 package com.example.discoverdishesapp
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 interface APIService {
 
 
+
+    companion object{
+        //servidor llamada
+        fun getInstance() : APIService {
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://dummyjson.com/recipes")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            return retrofit.create(APIService::class.java)
+
+        }
+    }
 }
